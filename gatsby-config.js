bc78d9493d15d593a2bee1d57f4ef73ff484d314
Dhillon Singh <dhillon.singh@wakatu.org>
2019-04-20 01:02:42 +1200

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Karaena`,
+    description: `Karaena Vincent Material from scratch.`,
+    author: `Navtej Dhillon`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-sass',
+    {
+    resolve:'gatsby-source-drupal',
+    options: {
+      baseUrl:'http://192.168.1.83',
+      apiBase:'jsonapi',
+      basicAuth:{
+        username: process.env.DRUPAL_USERNAME,
+        password: process.env.DRUPAL_PASSWORD,
+      },
+    },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -24,7 +36,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+       // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
